4c400ca7578f507b2cf918b587ea0c0627b8688c
googlepriya <gokulakavya123@gmail.com>
2024-08-26 16:57:08 +0530

    add and remove cart commit

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,11 +5,23 @@ class ProductPage:
     def __init__(self,driver):
         self.driver = driver
         self.click_proudct_sort = (By.XPATH,"//select[@class='product_sort_container']")
+
+        self.sauce_labs_backpack_link = (By.ID,"item_4_title_link")
+        self.add_to_cart_button = (By.ID,"add-to-cart")
+        self.remove_from_card = (By.ID,"remove")
     
     def sortProduct(self,text):
         dropdown_element = self.driver.find_element(*self.click_proudct_sort)
         select = Select(dropdown_element)
         select.select_by_visible_text(text)
+
+    def addToCart(self):
+        self.driver.find_element(*self.sauce_labs_backpack_link).click()
+        self.driver.find_element(*self.add_to_cart_button).click()
+
+    def removeItem(self):
+        self.driver.find_element(*self.remove_from_card).click()
+
 
 
         
--- test/test_product.py
+++ test/test_product.py
@@ -22,12 +22,33 @@ def test_filter(setup):
     login_page.login("standard_user","secret_sauce")
 
     #Test sort the product
-    sort_product =  ProductPage(driver)
-    sort_product.sortProduct("Name (Z to A)")
+    test_product =  ProductPage(driver)
+    test_product.sortProduct("Name (Z to A)")
     time.sleep(2)
 
-    sort_product.sortProduct("Price (low to high)")
+    test_product.sortProduct("Price (low to high)")
     time.sleep(2)
 
-    sort_product.sortProduct("Price (high to low)")
-    time.sleep(4)
+    test_product.sortProduct("Price (high to low)")
+    time.sleep(2)
+
+    test_product.sortProduct("Name (A to Z)")
+    time.sleep(2)
+
+    #Add to Cart
+    test_product.addToCart()
+    time.sleep(2)
+
+    #Assrt to check item is added to the cart
+    shop_card_badge = driver.find_element(By.XPATH,"//span[@class='shopping_cart_badge']").text
+    assert "1" in shop_card_badge, "Item is not added to the card"
+
+    #Remove item from the cart
+    test_product.removeItem()
+    time.sleep(2)
+    #Assrt to check item is removed from the cart
+    add_to_card = driver.find_element(By.ID,"add-to-cart").text
+    assert "Add to cart" in add_to_card, "Item is not removed from the cart"
+
+
+
